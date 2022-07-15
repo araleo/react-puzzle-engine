b6eb9d968d7e7f3deaadad4c772943d8dbc07ac8
Leonardo <mendes.lnr@gmail.com>
2022-07-15 00:10:48 -0300

    Adding guessing game example

--- src/examples/guessing/Guessing.tsx
+++ src/examples/guessing/Guessing.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useLifes from '../../hooks/use-lifes/use-lifes';
+import useScore from '../../hooks/use-score/use-score';
+import { getRandomIntInRange } from '../../lib/rng';
 
 const GuessingGame = () => {
-  return <h1>Guessing game</h1>;
+  const [secret, setSecret] = useState<number>(getRandomIntInRange(1, 100));
+  const [guess, setGuess] = useState<number>(0);
+  const [result, setResult] = useState<string>('');
+
+  const { lifes, takeOneLife, resetLifes } = useLifes({ start: 10 });
+  const { score, addScore, resetScore } = useScore();
+
+  const handleGuessChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = +e.target.value;
+    if (newValue >= 0 && newValue <= 100) {
+      setGuess(+newValue);
+    }
+  };
+
+  const handleGuess = () => {
+    let newResult: string;
+    if (guess === secret) {
+      newResult = 'You win!';
+      addScore(10 * lifes);
+    } else if (guess < secret) {
+      newResult = 'Too low';
+      takeOneLife();
+    } else {
+      newResult = 'Too high';
+      takeOneLife();
+    }
+    setResult(newResult);
+  };
+
+  const handleReset = () => {
+    setSecret(getRandomIntInRange(1, 100));
+    setGuess(0);
+    setResult('');
+    resetLifes();
+    resetScore();
+  };
+
+  return (
+    <>
+      <h1>Guessing game</h1>
+      <p>Tries: {lifes}</p>
+      <p>Score: {score}</p>
+
+      <p>I am thinking of a number. Can you guess which one?</p>
+      <p>{result}</p>
+
+      <input
+        type='number'
+        min={1}
+        max={100}
+        value={guess}
+        onChange={handleGuessChange}
+      />
+      <button onClick={handleGuess}>Guess</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
 };
 
 export default GuessingGame;
