5cd256557e42a02b6b0d35061d38c4e6420040e7
Leonardo <mendes.lnr@gmail.com>
2022-07-17 00:37:15 -0300

    Only displaying already clicked cells

--- src/examples/minesweeper/Minesweeper.tsx
+++ src/examples/minesweeper/Minesweeper.tsx
@@ -13,6 +13,7 @@ const BOMBS_AMOUNT = 10;
 const Minesweeper = () => {
   const [grid, setGrid] = useState<number[][]>(make2dArray(SIZE, SIZE));
   const [bombCells, setBombCells] = useState<[number, number][]>([]);
+  const [revealed, setRevealed] = useState<[number, number][]>([]);
 
   useEffect(() => {
     const bombs = getNRandomXYs(BOMBS_AMOUNT, 0, SIZE - 1, 0, SIZE - 1);
@@ -24,18 +25,35 @@ const Minesweeper = () => {
     setGrid(newGrid);
   }, [bombCells]);
 
-  console.log(bombCells);
+  const handleCellClick = (row: number, col: number) => {
+    if (!areCoordsOnArray([row, col], revealed)) {
+      const updated = [...revealed];
+      updated.push([row, col]);
+      setRevealed(updated);
+    }
+  };
 
   return (
     <>
       <h1>Minesweeper</h1>
       {grid.map((row, rowIdx) => (
         <div key={rowIdx} className={style.row}>
-          {row.map((column, columnIdx) => (
-            <div key={columnIdx} className={style.cell}>
-              {areCoordsOnArray([rowIdx, columnIdx], bombCells) ? 'X' : column}
-            </div>
-          ))}
+          {row.map((cell, colIdx) => {
+            const coords: [number, number] = [rowIdx, colIdx];
+            const isBomb = areCoordsOnArray(coords, bombCells);
+            const show = areCoordsOnArray(coords, revealed);
+            const display = isBomb ? 'X' : cell;
+
+            return (
+              <div
+                key={colIdx}
+                onClick={() => handleCellClick(rowIdx, colIdx)}
+                className={style.cell}
+              >
+                {show ? display : ''}
+              </div>
+            );
+          })}
         </div>
       ))}
     </>
